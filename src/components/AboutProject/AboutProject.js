5d8371055dc72131a65a7f2ac26e5da9345134db
p1d3c <mrkomnot@yandex.ru>
2022-07-31 19:27:35 +0300

    feature/add AboutProject

---
+++ src/components/AboutProject/AboutProject.js
@@ -0,0 +1,37 @@
+import './AboutProject.css';
+
+function AboutProject() {
+  return (
+    <div className='project'>
+      <h2 className='project__title'>О проекте</h2>
+      <div className='project__container'>
+        <div className='project__element'>
+          <h3 className='project__subtitle'>Дипломный проект включал 5 этапов</h3>
+          <p className='project__paragraph'>
+            Составление плана, работу над бэкендом, вёрстку, добавление 
+            функциональности и финальные доработки.
+          </p>
+        </div>
+        <div className='project__element'>
+          <h3 className='project__subtitle'>На выполнение диплома ушло 5 недель</h3>
+          <p className='project__paragraph'>
+            У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было
+            соблюдать, чтобы успешно защититься.
+          </p>
+        </div>
+      </div>
+      <div className='project__progress'>
+        <div className='project__line'>
+          <p className='project__text'>1 неделя</p>
+        </div>
+        <div className='project__line project__line_front'>
+          <p className='project__text project__text_front'>4 недели</p>
+        </div>
+        <p className='project__text project__text_architecture'>Back-end</p>
+        <p className='project__text project__text_architecture'>Front-end</p>
+      </div>
+    </div>
+  )
+}
+
+export default AboutProject;
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,11 +1,17 @@
 import './Main.css';
 import Promo from '../Promo/Promo';
+import AboutProject from '../AboutProject/AboutProject';
 
 function Main() {
   return (
-    <section className='banner'>
-      <Promo />
-    </section>
+    <>
+      <section className='banner'>
+        <Promo />
+      </section>
+      <section>
+        <AboutProject />
+      </section>
+    </>
   )
 }
 
